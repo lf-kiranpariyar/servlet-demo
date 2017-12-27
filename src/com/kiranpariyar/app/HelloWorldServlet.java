dfaf12b275b0d7de2b8594720e0cd379a546d1db
lf-kiranpariyar <kiranpariyar@lftechnology.com>
2017-12-27 21:48:13 +0545

    Create HelloWorldServlet

---
+++ src/com/kiranpariyar/app/HelloWorldServlet.java
@@ -0,0 +1,25 @@
+package com.kiranpariyar.app;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by kiran on 12/27/17.
+ */
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.print("<h1>Hello World, First Servlet program</h1>");
+        out.flush();
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kiran
+  Date: 12/27/17
+  Time: 8:51 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Hello World</title>
+  </head>
+  <body>
+  Servlet Hello World
+  </body>
+</html>
